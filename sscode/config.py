12d93ca4e0550716760ff358b22e3d38450f76b7
javitausia <jtausiahoyal@gmail.com>
2021-05-07 14:01:39 +0200

    config python file updated

--- sscode/config.py
+++ sscode/config.py
@@ -4,5 +4,5 @@ data_path = '/home/javitausia/Documentos/geocean-nz-ss/data' # change for local 
 
 default_location = (173.9,-40.5) # location at NZ
 
-default_region = (140,190,-20,-60) # region of NZ
+default_region = (140,190,-20,-70) # region of NZ
 default_region_reduced = (165,185,-30,-50) # reduced region of NZ
